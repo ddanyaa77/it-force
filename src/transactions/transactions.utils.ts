5d68976d18261f87f4914345ea0a0e8e4c37bd7d
danya <daniil.druzhina@1win.pro>
2024-09-19 00:28:21 +0300

    delete unnecessary file

--- src/transactions/transactions.utils.ts
+++
@@ -1,9 +0,0 @@
-import { TransactionAction } from './types/transaction.types';
-
-export const TransactionActionCompensationMap: Record<
-  TransactionAction,
-  TransactionAction
-> = {
-  [TransactionAction.DEDUCTION]: TransactionAction.REPLENISHMENT,
-  [TransactionAction.REPLENISHMENT]: TransactionAction.DEDUCTION,
-};
